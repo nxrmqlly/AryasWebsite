f17349b77a10655dc531c738ee072ccf4b844d2b
Arya Honarmand <122024468+not-arya@users.noreply.github.com>
2023-09-19 15:13:16 +0330

    Update

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,35 +1,34 @@
-function fetchSpotifyData() {
-  fetch("https://api.lanyard.rest/v1/users/1147999469854523414/")
-      .then((response) => response.json())
-      .then((data) => {
-          const spotifyData = data.data.spotify;
-          if (spotifyData) {
-              const song = spotifyData.song;
-              const artist = spotifyData.artist;
-              const album = spotifyData.album;
-              const albumArtUrl = spotifyData.album_art_url;
+function fetchData() {
+	fetch("https://api.lanyard.rest/v1/users/1147999469854523414/")
+		.then((response) => response.json())
+		.then((data) => {
+			const spotifyData = data.data.spotify;
+			if (spotifyData) {
+				const song = spotifyData.song;
+				const artist = spotifyData.artist;
+				const album = spotifyData.album;
+				const albumArtUrl = spotifyData.album_art_url;
 
-              // Define your desired colors
-              const songColor = "#81A1C1"; // Replace with your preferred color
-              const artistColor = "#A3BE8C"; // Replace with your preferred color
-              const albumColor = "#B48EAD"; // Replace with your preferred color
+				const songColor = "#81A1C1";
+				const artistColor = "#A3BE8C";
+				const albumColor = "#B48EAD";
 
-              // Update the HTML with colored text
-              document.querySelector(".album-art").src = albumArtUrl;
-              document.querySelector(".song-title").innerHTML = `<span style="color: ${songColor};">${song}</span>`;
-              document.querySelector(".artist-name").innerHTML = `By <span style="color: ${artistColor};">${artist}</span>`;
-              document.querySelector(".album-name").innerHTML = `On <span style="color: ${albumColor};">${album}</span>`;
-          } else {
-              document.querySelector(".other-info").innerHTML = "Not listening to Spotify.";
-          }
-      })
-      .catch((error) => {
-        document.querySelector(".other-info").innerHTML = `<span style="color: #FF6961;">Error while fetcing.</span>`;
-      });
+				const albumArt = document.querySelector(".album-art");
+				albumArt.src = albumArtUrl;
+
+				document.querySelector(".song-title").innerHTML = `<span style="color: ${songColor};">${song}</span>`;
+				document.querySelector(".artist-name").innerHTML = `By <span style="color: ${artistColor};">${artist}</span>`;
+				document.querySelector(".album-name").innerHTML = `On <span style="color: ${albumColor};">${album}</span>`;
+			} else {
+				document.querySelector(".other-info").innerHTML = "Not listening to Spotify.";
+			}
+		})
+		.catch((error) => {
+			console.error("[ERROR] Could not fetch/find Spotify.");
+			document.querySelector(".other-info").innerHTML = "Error while fetching.";
+		});
 }
 
-// Fetch data initially
-fetchSpotifyData();
+fetchData();
 
-// Fetch data every 5 seconds
-setInterval(fetchSpotifyData, 5000);
+setInterval(fetchData, 5000);
